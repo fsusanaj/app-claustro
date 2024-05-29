8c811e5e5ec73108e8642acb8707ea83ba58f8bb
fsusanaj <fsusanaj@gmail.com>
2024-05-29 15:21:41 -0600

    Inicio del Proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: '',
     pathMatch: 'full'
   },
+
 ];
 
 @NgModule({
